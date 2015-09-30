ea8c2703f9bd62747c113ccb14e5af64fe184ff8
rtduany <rtduany@gmail.com>
2015-09-30 17:54:05 +0300

    added last name and email fields

--- signUps/models.py
+++ signUps/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class SignUp(models.Model):
 	first_name = models.Charfield(max_length=120, null=True, blank=True)
+	last_name = models.Charfield(max_length=120, null=True, blank=True)
+	email =
+
