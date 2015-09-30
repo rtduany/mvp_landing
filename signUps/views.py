d93b7243cbdcf7eab09e5bda6d4a88606537cf64
rtduany <rtduany@gmail.com>
2015-10-01 01:51:22 +0300

    added code to home function

--- signUps/views.py
+++ signUps/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, render_to_response, RequestContext
 
 # Create your views here.
 def home(request):
-	return #something
+	return render_to_response("signup.html", locals(), context_instance=RequestContext(request))
